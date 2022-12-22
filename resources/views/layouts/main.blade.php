4b4eb2883a55132d11b7e5688b099013a18783d6
Krona Suporte <anderson.bastos@kronamaxxi.com.br>
2022-12-22 16:09:02 -0300

    alteracoes no projeto

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,28 +3,64 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>@yeld('title')</title>
 
         <!-- CSS da Aplicação -->
+        <link rel="stylesheet" href="/css/estilo.css">
        
-
         <!-- Font da Aplicação -->
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
 
         <!-- Bootstrap da Aplicação -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
+        <!-- Scripts da Aplicação -->
+        <script href="/js/scripts.js"></script>
     </head>
-    <body>
+    <body class="corpo-geral">
         @yield('content')
 
-
-
-            
-
-        <footer>
-            <p> Krona Ferramentas &copy; 2022</p>
+        <header class="cabecalho-aplicacao">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#"><img src="/images/icon/principal.png">&nbsp;AGM Solutions</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Avaliações</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                        Relatórios
+                      </a>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Lista de TMS</a>
+                        <a class="dropdown-item" href="#">Ramais</a>
+                        <a class="dropdown-item" href="#">Resultados Consolidados</a>
+                      </div>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link">Acessar ADM</a>
+                    </li>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                </div>
+              </nav>
+        </header>
+        <div class="container">
+        
+        </div>
+        <footer class="rodape-aplicacao">
+            <p class="texto-rodape"> Krona Ferramentas &copy; 2022</p>
         </footer>
 
 
