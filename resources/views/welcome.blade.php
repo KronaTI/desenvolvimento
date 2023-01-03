f68c696c794b88d86462e6c4db30d7a0a0d03994
Krona Suporte <anderson.bastos@kronamaxxi.com.br>
2023-01-03 16:26:39 -0300

    alterações gerais

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,41 @@
 
 @section('title', 'AGM Solutions')
 
-
-
 @section('content')
 
+<div class="card" style="width: 18rem;">
+    <img src="/images/figuras-projeto/quadro-um.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h4>Qualidade</h4>
+      <p class="card-text">Textos para exemplificar</p>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="/images/figuras-projeto/quadro-um.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h4>Qualidade</h4>
+      <p class="card-text">Textos para exemplificar</p>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <img src="/images/figuras-projeto/quadro-um.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h4>Qualidade</h4>
+      <p class="card-text">Textos para exemplificar</p>
+    </div>
+  </div>
     
 
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
 @endsection
