e9a424417b3d0349fc0947787a0f0e17d87d9ba1
Krona Suporte <anderson.bastos@kronamaxxi.com.br>
2023-01-30 18:25:21 -0300

    Alterações na pagina principal e adica pagina de TMS

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="favcon" href="/images/favcon.png" type="image/png">
         <title>@yield('title')</title>
 
         <!-- CSS da Aplicação -->
